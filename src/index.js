0456c7319d7ea5346384c96040edffacb38f3a56
Matthew Burroughs <matthew@trala.com>
2023-02-15 07:58:07 -0600

    Adding updates for on-site

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ const Boom = require('boom');
 const Hapi = require('@hapi/hapi');
 const HapiSwagger = require('hapi-swagger');
 const Inert = require('@hapi/inert');
-const Joi = require('joi');
+const JoiBase = require('joi');
+const JoiDate = require ('@hapi/joi-date');
 const Pack = require('../package');
 const Vision = require('@hapi/vision');
 
 const { Pool } = require('pg');
 const { v4: uuidv4 } = require('uuid');
+
+const Joi = JoiBase.extend(JoiDate); // Extend Joi with Joi Date
 
 // Database
 const pool = new Pool({
@@ -67,10 +70,16 @@ const init = async () => {
                     first_name: Joi.string().min(1).max(128).optional(),
                     middle_initial: Joi.string().min(1).max(128).optional(),
                     last_name: Joi.string().min(1).max(128).optional(),
-                    birth_date: Joi.date().optional(),
+                    birth_date: Joi.date()
+                        .format('YYYY-MM-DD').optional(),
                     country_code: Joi.string().min(1).max(8).optional(),
                     phone_number: Joi.string().min(1).max(24).optional(),
-                })
+                }),
+                failAction: async (request, h, err) => {
+                    // Log and respond with the full error.
+                    console.error(err);
+                    throw err;
+                },
             }
         },
         handler: async (request, h) => {
@@ -86,6 +95,13 @@ const init = async () => {
                 phone_number: _.get(payload, 'phone_number') || null,
             };
             try {
+                // Ensure that the date provided is not in the future
+                if (new Date(contact.birth_date) > new Date()) {
+                    const boomError = Boom.badRequest('"birth_date" must be in the past.');
+                    boomError.output.payload.validation = { source: 'payload', keys: ['birth_date'] };
+                    return boomError;
+                }
+
                 // Let's check to make sure this email address isn't already taken.
                 const validRes = await pool.query("SELECT * FROM contacts WHERE email = $1", [ contact.email ]);
                 if (validRes.rows.length > 0) return Boom.conflict('That email address already exists');
@@ -112,10 +128,15 @@ const init = async () => {
                     first_name: Joi.string().min(1).max(128).optional(),
                     middle_initial: Joi.string().min(1).max(128).optional(),
                     last_name: Joi.string().min(1).max(128).optional(),
-                    birth_date: Joi.date().optional(),
+                    birth_date: Joi.date().format('YYYY-MM-DD').optional(),
                     country_code: Joi.string().min(1).max(8).optional(),
                     phone_number: Joi.string().min(1).max(24).optional(),
-                })
+                }),
+                failAction: async (request, h, err) => {
+                    // Log and respond with the full error.
+                    console.error(err);
+                    throw err;
+                },
             }
         },
         handler: async (request, h) => {
@@ -131,6 +152,13 @@ const init = async () => {
             if (_.get(payload, 'country_code')) _.set(updatedContact, 'country_code', payload.country_code);
             if (_.get(payload, 'phone_number')) _.set(updatedContact, 'phone_number', payload.phone_number);
             try {
+                // Ensure that the date provided is not in the future
+                if (new Date(updatedContact.birth_date) > new Date()) {
+                    const boomError = Boom.badRequest('"birth_date" must be in the past.');
+                    boomError.output.payload.validation = { source: 'payload', keys: ['birth_date'] };
+                    return boomError;
+                }
+
                 // Let's make sure this exists
                 const findRes = await pool.query("SELECT * FROM contacts WHERE id = $1", [ contactId ]);
                 if (findRes.rows.length === 0) return Boom.badRequest('There is no contact with that identifier.');
@@ -145,7 +173,37 @@ const init = async () => {
                 const contact = _.merge(existingContact, updatedContact);
                 const updateRes = await pool.query(
                     "UPDATE contacts SET email=$2, first_name=$3, middle_initial=$4, last_name=$5, birth_date=$6, country_code=$7, phone_number=$8 WHERE id=$1 RETURNING *",
-                    [ contactId, contact.email, contact.first_name, contact.middle_initial, contact.last_name, contact.birth_date, contact.country_code, contact.phone_number],
+                    [ contactId, contact.email, contact.first_name, contact.middle_initial, contact.last_name, contact.birth_date, contact.country_code, contact.phone_number ],
+                );
+                return updateRes.rows[0];
+            } catch (error) {
+                console.log(error);
+                return Boom.internal(error);
+            }
+        }
+    });
+    server.route({
+        method: 'DELETE',
+        path: '/v1/contact/{id}',
+        options: {
+            cors: true,
+            tags: ['api'],
+            validate: {
+                params: Joi.object({
+                    id: Joi.string().min(36).max(36).required(),
+                }),
+            }
+        },
+        handler: async (request, h) => {
+            const { id: contactId } = request.params;
+            try {
+                // Let's make sure this exists
+                const findRes = await pool.query("SELECT * FROM contacts WHERE id = $1", [ contactId ]);
+                if (findRes.rows.length === 0) return Boom.badRequest('There is no contact with that identifier.');
+
+                const updateRes = await pool.query(
+                    "UPDATE contacts SET is_active = $2 WHERE id=$1 RETURNING *",
+                    [ contactId, false ],
                 );
                 return updateRes.rows[0];
             } catch (error) {
